bcaf4b757e9ebd189e3edf6d57bb0c43c85211e2
dev <hizlle1817@gmail.com>
2021-03-02 15:11:52 +0400

    solving tasks

--- src/main/java/Practice_tasks/Basics_of_software_code_dev_1/Solving.java
+++ src/main/java/Practice_tasks/Basics_of_software_code_dev_1/Solving.java
@@ -1,4 +1,52 @@
 package Practice_tasks.Basics_of_software_code_dev_1;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Solving {
+    public static void main(String[] args) {
+
+
+    }
+
+
+    static int findFunction(int a, int b, int c) {
+        return ((a - 3) * b / 2) + c;
+    }
+
+
+    static int evaluateExpressionValues1(int a, int b, int c) {
+        return a;
+    }
+
+
+    static int evaluateExpressionValues2(int x, int y) {
+
+        return x;
+
+    }
+
+    static double swapExpression(double x) {
+        double y = 0.0;
+        double n = (int) x;
+        return y = (x * 1000) % 1000 + (int) x / 1000.0;
+
+    }
+
+    static HashMap<Integer, String> findFunction(int sec) {
+        HashMap<Integer, String> map;
+
+
+        String hours = "hour";
+        String minutes = "min";
+        String seconds = "sec";
+
+        return null;
+    }
+
+
+    static boolean thePointBelongsTo(int x, int y) {
+        return x <= 4 && x >= -4 && y <= 4 && y >= -3;
+    }
+
 }
